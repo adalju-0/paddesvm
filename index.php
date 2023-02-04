839d99d43d710c71229634373cafcfbfabcde89b
adamljuslinder <adamljuslinder@hotmail.com>
2023-02-04 12:40:50 +0100

    first real commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html>
+    <?php include 'functions/config.php';?>
+    <head> 
+        <meta charset="UTF-8">
+    </head> 
+    <body>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+            <div id="header">
+            This is a page
+
+
+            </div>
+    </body>
+</html>     
